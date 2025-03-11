77398bc04f46fa0c4b4e7d52398053e428afb26d
Ravi Akshintala <raviakshintalagithub.com@Ravis-Laptop.local>
2025-03-11 17:11:35 +0530

    [Ubuntu-20] updated the  google cloud core

--- images/ubuntu/scripts/build/install-ruby.sh
+++ images/ubuntu/scripts/build/install-ruby.sh
@@ -14,9 +14,8 @@ apt-get install ruby-full
 if is_ubuntu20; then
     gem install public_suffix -v 5.1.1
     
-    # Install google-cloud-errors gem pinned to version 1.4.0
-    gem install google-cloud-errors -v 1.4.0
-fi
+    # Install google-cloud-errors 
+   gem install google-cloud-core
 
 # Install ruby gems from toolset
 gems_to_install=$(get_toolset_value ".rubygems[] .name")
